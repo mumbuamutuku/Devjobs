c30d521fc4a1725511e62baacd6789da32a2136d
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-06-01 23:21:37 +0100

    completed header, filter and job section

---
+++ dev-jobs/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderArea/>
+</template>
+
+<script>
+import  HeaderArea from './components/HeaderArea.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderArea
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ dev-jobs/src/components/HeaderArea.vue
@@ -0,0 +1,318 @@
+<template>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="/images/logo.svg" alt="" srcset="">
+            </div>
+            <div class="switcher">
+                
+               
+                <label id="switch" class="switch">
+                   <img src="/images/icon-sun.svg" alt="" style="margin-left: -11px;">
+                    <input type="checkbox" @change="toggleTheme" id="slider">
+                    <span class="slider round"></span>
+                    <img src="/images/icon-moon.svg" alt="" style="    margin-left: 66px;">
+                    
+                </label>
+               
+            </div>
+        </nav>
+        
+    </header>
+    <main>
+        <div class="search-section">
+            <div class="search-by-title">
+                <img src="/images/icon-search.svg" alt="">
+                <input type="search" name="" id="" placeholder="Filter by title, companies, expertise...">
+            </div>
+            <div class="search-by-location">
+                <img src="/images/icon-location.svg" alt="">
+                <input type="search" name="" id="" placeholder="Filter by location...">
+            </div>
+            <div class="search-button">
+                <input type="checkbox" name="" id="">
+                <p>Full Time only</p>
+                <button>Search</button>
+            </div>
+        </div>
+        <div class="job-section">
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/officelite.svg" alt="">
+                </div>
+                <div style="display:flex">
+                    <p>5 hrs ago &#x2022;</p>
+                    <p>Full time</p>
+
+                    
+                </div>
+                <div>
+                    <h2>Senior Software Engineer</h2>
+                </div>
+                <div>
+                    <p>Vector</p>
+                </div>
+                <div>
+                    <h4>United Kingdom</h4>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'headerArea',
+    data () {
+        return{
+
+        }
+    },
+    methods:{
+        setTheme(themeName) {
+        localStorage.setItem('theme', themeName);
+        document.documentElement.className = themeName;
+        },
+        toggleTheme() {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                this.setTheme('theme-light');
+            } 
+            else {
+                this.setTheme('theme-dark');
+            }
+        },
+        setThemeNow () {
+            if (localStorage.getItem('theme') === 'theme-dark') {
+                this.setTheme('theme-dark');
+                document.getElementById('slider').checked = false;
+            } else {
+                this.setTheme('theme-dark');
+                document.getElementById('slider').checked = true;
+            }
+        }
+    },
+    mounted(){
+        this.setThemeNow()
+    }
+}
+</script>
